71e5b1e52c0ec32238d3db4125b16a953c22c713
Bruno C Ambrosio <bruno.car.ambrosio@gmail.com>
2020-06-30 17:35:55 -0300

    Initial Commit

---
+++ Shared/TodoManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoManagerApp.swift
+//  Shared
+//
+//  Created by Bruno Cardoso Ambrosio on 30/06/20.
+//
+
+import SwiftUI
+
+@main
+struct TodoManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
